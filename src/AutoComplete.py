6562d006741096130c98ce024f5951ce09c593e5
nitsan <nitsan.soffair@gmail.com>
2022-02-26 20:03:19 +0200

    tokenize_sentences

--- src/AutoComplete.py
+++ src/AutoComplete.py
@@ -7,3 +7,23 @@ class AutoComplete:
         sentences = [s.strip() for s in sentences]
         sentences = [s for s in sentences if len(s) > 0]
         return sentences
+
+    def tokenize_sentences(self, sentences):
+        tokenized_sentences = []
+        for sentence in sentences:
+            sentence = sentence.lower()
+            words = sentence.split(' ')
+            tokenized = []
+            for index in range(len(words)):
+                word = words[index]
+                if len(word) == 0:
+                    continue
+                if word[-3:] == "...":
+                    tokenized.append(word[:-3]), tokenized.append(word[-3:])
+                elif word[-1] in list(".;?"):
+                    tokenized.append(word[:-1]), tokenized.append(word[-1])
+                    index += 1
+                else:
+                    tokenized.append(word)
+            tokenized_sentences.append(tokenized)
+        return tokenized_sentences
--- src/tests/AutoComplete.py
+++ src/tests/AutoComplete.py
@@ -43,6 +43,78 @@ class AutoCompleteTest(unittest.TestCase):
                 for elem in result:
                     self.assertEqual(True, len(elem) == 1)
 
+    def test_tokenize_sentences(self):
+        auto_complete = AutoComplete()
+        target = auto_complete.tokenize_sentences
+        test_cases = [
+            {
+                "name": "notebook example",
+                "input": ["Sky is blue.", "Leaves are green.", "Roses are red."],
+                "expected": [
+                    ["sky", "is", "blue", "."],
+                    ["leaves", "are", "green", "."],
+                    ["roses", "are", "red", "."],
+                ],
+            },
+            {"name": "no sentence", "input": [], "expected": []},
+            {
+                "name": "one sentence with ;",
+                "input": ["Grass is greener;"],
+                "expected": [["grass", "is", "greener", ";"]],
+            },
+            {
+                "name": "two sentences, one in CAPS",
+                "input": ["Space if infinite.", "OR IS IT?"],
+                "expected": [["space", "if", "infinite", "."], ["or", "is", "it", "?"]],
+            },
+            {
+                "name": "Sentence with empty string",
+                "input": ["Next sentence is empty string.", "", "This one is not empty."],
+                "expected": [
+                    ["next", "sentence", "is", "empty", "string", "."],
+                    [],
+                    ["this", "one", "is", "not", "empty", "."],
+                ],
+            },
+            {
+                "name": "Sentence with space",
+                "input": ["Next sentence is full of spaces.", "   ", "This one is full."],
+                "expected": [
+                    ["next", "sentence", "is", "full", "of", "spaces", "."],
+                    [],
+                    ["this", "one", "is", "full", "."],
+                ],
+            },
+            {
+                "name": "long sentence",
+                "input": [
+                    "Really really long sentence. It is very long indeed; so long..."
+                ],
+                "expected": [
+                    [
+                        "really",
+                        "really",
+                        "long",
+                        "sentence",
+                        ".",
+                        "it",
+                        "is",
+                        "very",
+                        "long",
+                        "indeed",
+                        ";",
+                        "so",
+                        "long",
+                        "...",
+                    ]
+                ],
+            },
+        ]
+        for test_case in test_cases:
+            result = target(test_case["input"])
+            self.assertEqual(True, isinstance(result, type(test_case["expected"])))
+            self.assertEqual(True, len(result) == len(test_case["expected"]))
+            self.assertEqual(True, test_case["expected"] == result)
 
 if __name__ == '__main__':
     unittest.main()
